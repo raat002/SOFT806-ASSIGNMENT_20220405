cf55984b9abce062c07a3ea73de3400168f87f40
RAF ATIENZA <raat002@ess.ais.ac.nz>
2023-03-04 20:58:33 +1300

    SOFT806 Assignment 1st commit

---
+++ IdentityDemo/Areas/Data/AppUsers.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace IdentityDemo.Areas.Data
+{
+    public class AppUsers: IdentityUser
+    {
+        public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    }
+}
